2a7b85be8d55b2f0edc0cec8d1b7f5c4ce00f660
snsonalinayak@gmail.com <89394473+snsonalinayak@users.noreply.github.com>
2023-06-21 12:21:10 +0530

    resume file

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ import sonaliResume from '../images/Sonali Resume.png'
 const Resume = () => {
   return (
     <div className="container d-flex align-items-center justify-content-center mh-100" >
-        resume
+        
         <img className='align-self-start my-5' src={sonaliResume} style={{height:"50rem"}} />
 
     </div>
